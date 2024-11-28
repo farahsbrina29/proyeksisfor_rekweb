a4d8b9465eddcef729ec412540ae66f75dee4038
alfian230603 <alf2363progamer@gmail.com>
2024-11-28 18:07:09 +0800

    Update : HeroSection

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -5,11 +5,18 @@ const HeroSection = () => {
     <div
       className="relative h-screen bg-cover"
       style={{
-        backgroundImage: `url('/assets/signup.jpg')`,
-        backgroundPosition: 'center 90%', // Geser ke atas dengan mengatur posisi vertikal
+        backgroundImage: `url('/assets/signup.jpg')`,  // Ganti dengan path gambar yang diinginkan
+        backgroundPosition: 'center 70%',
+        backgroundAttachment: 'fixed',  // Efek parallax
+        overflow: 'hidden',  // Menyembunyikan bagian gambar yang keluar dari rounded corners
+        boxShadow: '0 15px 30px rgba(0, 0, 0, 0.2)',  // Efek bayangan di bawah
       }}
     >
-      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div
+        className="absolute inset-0 bg-black bg-opacity-50"
+        style={{
+        }}
+      ></div>
       <div className="relative z-10 flex flex-col items-center justify-center h-full text-center text-white">
         <h1 className="text-6xl md:text-7xl font-bold">
           Found Your Scholar Dream
