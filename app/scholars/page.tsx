ec3cf05acb786ee9c55d67a1a9b55981a15fa8c3
gerynsb <118332285+gerynsb@users.noreply.github.com>
2024-12-04 11:54:27 +0800

    Update : memisahkan komponen dari scholars

--- app/scholars/page.tsx
+++ app/scholars/page.tsx
@@ -7,23 +7,19 @@ import "react-toastify/dist/ReactToastify.css";
 import { auth, db } from "@/lib/firebaseConfig";
 import { collection, getDocs } from "firebase/firestore";
 import { onAuthStateChanged } from "firebase/auth";
+import { getScholarshipStatus } from "@/utility/scholarshipdatautility";
 import "../../styles/scholarpage.css";
-import {
-  parseCustomDate,
-  getScholarshipStatus,
-  formatCustomDate,
-} from "../../utility/scholarshipdatautility";
 import ScholarshipLoader from "@/components/Loader/scholarLoeader";
+import FilterSection from "@/components/scholars/FilterSection";
+import ScholarshipCard from "@/components/scholars/ScholarshipCard";
 
 export default function Home() {
   const [scholarships, setScholarships] = useState<any[]>([]);
-  const [selectedAll, setSelectedAll] = useState("All");
   const [selectedMasaAktif, setSelectedMasaAktif] = useState("");
   const [selectedJenis, setSelectedJenis] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
-  // Cek status autentikasi pengguna menggunakan Firebase
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setIsLoggedIn(!!currentUser);
@@ -31,7 +27,6 @@ export default function Home() {
     return () => unsubscribe();
   }, []);
 
-  // Ambil data beasiswa dari Firestore
   useEffect(() => {
     const fetchScholarships = async () => {
       try {
@@ -52,7 +47,6 @@ export default function Home() {
     fetchScholarships();
   }, []);
 
-  // Fungsi untuk handle klik pada card
   const handleCardClick = (id: string) => {
     if (isLoggedIn) {
       window.location.href = `/scholars/${id}`;
@@ -68,46 +62,19 @@ export default function Home() {
     }
   };
 
-  // Function to determine if the scholarship is active
-  const isActiveScholarship = (
-    tanggal_mulai: string,
-    tanggal_akhir: string
-  ): boolean => {
-    const today = new Date();
-    const start = parseCustomDate(tanggal_mulai);
-    const end = parseCustomDate(tanggal_akhir);
-    return today >= start && today <= end;
-  };
+  const filteredScholarships = scholarships.filter((scholarship) => {
+    const isMasaAktifValid =
+      selectedMasaAktif === "" ||
+      (selectedMasaAktif === "Sedang Berlangsung" &&
+        getScholarshipStatus(scholarship.tanggal_mulai, scholarship.tanggal_akhir) ===
+          "Active") ||
+      (selectedMasaAktif === "Akan Berakhir" &&
+        scholarship.tanggal_akhir <= new Date().toISOString());
+    const isJenisValid =
+      selectedJenis === "" || scholarship.kategori === selectedJenis;
 
-  const willEndSoonScholarship = (tanggal_akhir: string): boolean => {
-    const today = new Date();
-    const end = parseCustomDate(tanggal_akhir);
-    const daysLeft = (end.getTime() - today.getTime()) / (1000 * 60 * 60 * 24);
-
-    // Tampilkan hanya beasiswa yang akan berakhir dalam waktu 3 hari ke depan
-    return daysLeft > 0 && daysLeft <= 3;
-  };
-
-  // Filter scholarships based on dropdown selections
-  const filteredScholarships =
-    scholarships.length > 0
-      ? scholarships.filter((scholarship) => {
-          const isAll = selectedAll === "All";
-          const isMasaAktifValid =
-            selectedMasaAktif === "" ||
-            (selectedMasaAktif === "Sedang Berlangsung" &&
-              getScholarshipStatus(
-                scholarship.tanggal_mulai,
-                scholarship.tanggal_akhir
-              ) === "Active") ||
-            (selectedMasaAktif === "Akan Berakhir" &&
-              willEndSoonScholarship(scholarship.tanggal_akhir));
-          const isJenisValid =
-            selectedJenis === "" || scholarship.kategori === selectedJenis;
-
-          return isAll && isMasaAktifValid && isJenisValid;
-        })
-      : [];
+    return isMasaAktifValid && isJenisValid;
+  });
 
   return (
     <div className="scholar-page">
@@ -118,82 +85,29 @@ export default function Home() {
 
         <div className="container">
           {/* Filter Section */}
-          <div className="dropdown-container">
-            <div className="dropdown-wrapper">
-              <select
-                className="styled-dropdown"
-                value={selectedMasaAktif}
-                onChange={(e) => setSelectedMasaAktif(e.target.value)}
-              >
-                <option value="">Masa Aktif</option>
-                <option value="Sedang Berlangsung">Sedang Berlangsung</option>
-                <option value="Akan Berakhir">Akan Berakhir</option>
-              </select>
-              <div className="dropdown-icon">▼</div>
-            </div>
-
-            <div className="dropdown-wrapper">
-              <select
-                className="styled-dropdown"
-                value={selectedJenis}
-                onChange={(e) => setSelectedJenis(e.target.value)}
-              >
-                <option value="">Jenis Beasiswa</option>
-                <option value="Akademik">Akademik</option>
-                <option value="Non Akademik">Non Akademik</option>
-                <option value="Bantuan">Bantuan</option>
-                <option value="Penelitian">Penelitian</option>
-              </select>
-              <div className="dropdown-icon">▼</div>
-            </div>
-          </div>
+          <FilterSection
+            selectedMasaAktif={selectedMasaAktif}
+            setSelectedMasaAktif={setSelectedMasaAktif}
+            selectedJenis={selectedJenis}
+            setSelectedJenis={setSelectedJenis}
+          />
 
           {/* Cards Section */}
           {isLoading ? (
-            <ScholarshipLoader itemCount={6} /> // Jumlah skeleton cards
+            <ScholarshipLoader itemCount={6} />
           ) : (
             <div className="cards-container">
               {filteredScholarships.map((scholarship) => (
-                <div
+                <ScholarshipCard
                   key={scholarship.id}
-                  onClick={() => handleCardClick(scholarship.id)}
-                >
-                  <div className="card cursor-pointer">
-                    <h2 className="text-xl font-bold mb-2 text-black">
-                      {scholarship.nama_beasiswa}
-                    </h2>
-                    <p className="text-black text-sm mb-4">
-                      {formatCustomDate(scholarship.tanggal_mulai)} -{" "}
-                      {formatCustomDate(scholarship.tanggal_akhir)}
-                    </p>
-                    <p className="text-black text-sm mb-4">
-                      {scholarship.deskripsi.split(".")[0]}.
-                    </p>
-                    <div className="flex items-center space-x-2 mb-4">
-                      <span
-                        className={`px-2 py-1 rounded-full text-sm ${
-                          getScholarshipStatus(
-                            scholarship.tanggal_mulai,
-                            scholarship.tanggal_akhir
-                          ) === "Active"
-                            ? "bg-green-200 text-green-700"
-                            : "bg-red-200 text-red-700"
-                        }`}
-                      >
-                        {getScholarshipStatus(
-                          scholarship.tanggal_mulai,
-                          scholarship.tanggal_akhir
-                        )}
-                      </span>
-                      <span className="bg-gray-200 text-gray-700 px-2 py-1 rounded-full text-sm">
-                        {scholarship.kategori}
-                      </span>
-                    </div>
-                    <button className="text-blue-600 font-semibold hover:underline">
-                      Read More
-                    </button>
-                  </div>
-                </div>
+                  id={scholarship.id}
+                  nama_beasiswa={scholarship.nama_beasiswa}
+                  tanggal_mulai={scholarship.tanggal_mulai}
+                  tanggal_akhir={scholarship.tanggal_akhir}
+                  deskripsi={scholarship.deskripsi}
+                  kategori={scholarship.kategori}
+                  onClick={handleCardClick}
+                />
               ))}
             </div>
           )}
